f40ba517a54f369887d9c8ea85746dc543413dfa
Eduardo Duhart <66975371+Eduhartg@users.noreply.github.com>
2020-07-13 01:13:14 -0400

    Agregar archivos de proyecto.

---
+++ TodoApi.Test/TodoItemsTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TodoApi.Controllers;
+
+namespace TodoApi.Test
+{
+    [TestClass]
+    public class TodoItemsTest
+    {
+        [TestMethod]
+        public void PostTodoItem()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
